6b031042d55c2291c40b9cd0e71f28aa9331981c
Lam-Richard <richardlam2024@gmail.com>
2021-01-04 13:19:01 -0600

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-
 function playAudio(sound){
     var audio = new Audio(sound);
     audio.play()
 }
+
+const wdrum = document.getElementById('wdrum');
+const adrum = document.getElementById('adrum');
+const sdrum = document.getElementById('sdrum');
+const ddrum = document.getElementById('ddrum');
+const jdrum = document.getElementById('jdrum');
+const kdrum = document.getElementById('kdrum');
+const ldrum = document.getElementById('ldrum');
+
+wdrum.addEventListener('click', ()=> {
+    playAudio('./sounds/tom-1.mp3');
+});
+adrum.addEventListener('click', ()=> {
+    playAudio('./sounds/tom-2.mp3');
+});
+sdrum.addEventListener('click', ()=> {
+    playAudio('./sounds/tom-3.mp3');
+});
+ddrum.addEventListener('click', ()=> {
+    playAudio('./sounds/tom-4.mp3');
+});
+jdrum.addEventListener('click', ()=> {
+    playAudio('./sounds/snare.mp3');
+});
+kdrum.addEventListener('click', ()=> {
+    playAudio('./sounds/crash.mp3');
+});
+ldrum.addEventListener('click', ()=> {
+    playAudio('./sounds/kick-bass.mp3');
+});
+
+
+
